3082f4f2ab99f77f1f1a5c14a6b14abee42fbe33
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-21 03:57:56 -0800

    DBStroage implemented

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,10 @@ class BaseModel:
         else:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(vlaue, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
-                if 'id' not in kwagrs:
+                if 'id' not in kwargs:
                     self.id = str(uuid.uuid4())
                 if "created_at" not in kwargs:
                     self.created_at = datetime.now()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        """ FileStorage relationship"""
+
+        @property
+        def cities(self):
+            """return list of cities"""
+
+            from models import storage
+            from models.city import City
+
+            cities_list = []
+            cities_dict = storage.all(City)
+
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
