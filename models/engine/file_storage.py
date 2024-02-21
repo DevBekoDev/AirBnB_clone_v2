f2088c2544771e558b164f42146f504b078b488e
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2024-02-21 00:48:55 -0800

    delete feature added, updated all featrue to allow filtired search

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        search_class = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    search_class[key] = value
+            return search_class
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +54,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete object from __objects if it's there"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
+
